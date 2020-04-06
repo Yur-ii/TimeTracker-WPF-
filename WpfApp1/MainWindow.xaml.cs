660c0045cff1fc30335cfd85dbc9779a1620f6eb
Yurii Night <YuraYura1302@gmail.com>
2020-04-06 15:47:50 +0300

    remove not using libs

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,28 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Collections.ObjectModel;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
 using System.ComponentModel;
-using System.Runtime.CompilerServices;
-using System.Windows.Controls.Primitives;
 using System.Text.Json;
 using System.IO;
-using System.Globalization;
-using System.Threading;
-using System.Windows.Shell;
 
 namespace WpfApp1
 {
